4e4ac77b165eb7c5060513f21f1f7abee6cd2921
dreadlord-sedai <dahamifabbio@gmail.com>
2025-10-02 09:23:07 +0530

    react router

--- react-router/src/main.jsx
+++ react-router/src/main.jsx
@@ -3,6 +3,9 @@ import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router";
 import App from "./App";
 import Profile from "./Profile";
+import DefaultProfile from "./DefaultProfile";
+import Spinach from "./Spinach";
+import Popeye from "./Popeye";
 
 const router = createBrowserRouter([
   {
@@ -12,11 +15,16 @@ const router = createBrowserRouter([
   {
     path: "profile",
     element: <Profile />,
+    children: [
+      { index: true, element: <DefaultProfile /> },
+      { path: "spinach", element: <Spinach /> },
+      { path: "popeye", element: <Popeye /> },
+    ],
   },
 ]);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <RouterProvider router={router} />
-  </StrictMode>
+  </StrictMode>,
 );
